1093bffff1e64879bf2cdb4f6b2e4f87eab53690
Andrei P <popandrei230@yahoo.com>
2024-06-24 09:59:35 +0300

    Add shell commands infrastructure

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -3,11 +3,22 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	cmd, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmd = strings.TrimSuffix(cmd, "\n")
+	switch cmd {
+	default:
+		fmt.Fprintln(os.Stdout, fmt.Sprintf("%s: command not found", cmd))
+	}
 }

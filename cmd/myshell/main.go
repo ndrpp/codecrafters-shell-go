1466ba690af2369da056a10c5d7a554b17610734
Andrei P <popandrei230@yahoo.com>
2024-06-24 10:42:11 +0300

    Add echo command

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -13,19 +13,34 @@ func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 
-		cmd, err := reader.ReadString('\n')
+		input, err := reader.ReadString('\n')
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		cmd = strings.TrimSuffix(cmd, "\n")
+		input = strings.TrimSuffix(input, "\n")
+		cmd, args := splitByFirstSpace(input)
 		switch cmd {
-        case "exit 0":
-            os.Exit(0)
+		case "echo":
+			fmt.Fprintln(os.Stdout, args)
+
+		case "exit":
+			if args == "0" {
+				os.Exit(0)
+			} else {
+				fmt.Fprintln(os.Stdout, fmt.Sprintf("%s: command not found", cmd))
+			}
 
 		default:
 			fmt.Fprintln(os.Stdout, fmt.Sprintf("%s: command not found", cmd))
-			break
 		}
 	}
+}
+
+func splitByFirstSpace(s string) (string, string) {
+	parts := strings.SplitN(s, " ", 2)
+	if len(parts) == 2 {
+		return parts[0], parts[1]
+	}
+	return s, ""
 }
